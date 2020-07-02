7765a73da005f301d6cdbc8df1baed0f464cc6b7
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2020-07-02 11:42:28 +0200

    Add tests for term types

--- wikibase-datamodel-tests.ts
+++ wikibase-datamodel-tests.ts
@@ -2,6 +2,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import {
+	Fingerprintable,
 	Statement,
 	StatementMap,
 } from '.';
@@ -27,4 +28,62 @@ const douglasAdamsStatements: StatementMap = {
 const douglasAdamsStatementsWithoutArray: StatementMap = {
 	// @ts-expect-error
 	'P373': douglasAdamsStatement,
+};
+
+const douglasAdamsTerms: Fingerprintable = {
+	labels: {
+		en: {
+			language: 'en',
+			value: 'Douglas Adams',
+		},
+	},
+	descriptions: {
+		en: {
+			language: 'en',
+			value: 'English writer and humorist',
+		},
+		'en-gb': {
+			language: 'en-gb',
+			value: 'English writer and humourist',
+		},
+	},
+	aliases: {
+		en: [
+			{
+				language: 'en',
+				value: 'Douglas Noel Adams',
+			},
+			{
+				language: 'en',
+				value: 'Douglas Noël Adams',
+			},
+			{
+				language: 'en',
+				value: 'Douglas N. Adams',
+			},
+		],
+	},
+};
+
+const douglasAdamsTermsWithoutLanguage: Fingerprintable = {
+	labels: {
+		// @ts-expect-error
+		en: 'Douglas Adams',
+	},
+	descriptions: {
+		// @ts-expect-error
+		en: 'English writer and humorist',
+		// @ts-expect-error
+		'en-gb': 'English writer and humourist',
+	},
+	aliases: {
+		en: [
+			// @ts-expect-error
+			'Douglas Noel Adams',
+			// @ts-expect-error
+			'Douglas Noël Adams',
+			// @ts-expect-error
+			'Douglas N. Adams',
+		],
+	},
 };
